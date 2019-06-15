050147dce53ca3cb1bd368253582e595821b1afd
Eduardo M Soares <eduardo@heds.com.br>
2019-06-15 13:57:50 -0300

    Caixa Revisão versão inicial

--- ionicapp/src/pages/apicultura/colmeia-revisao-form/colmeia-revisao-form.ts
+++ ionicapp/src/pages/apicultura/colmeia-revisao-form/colmeia-revisao-form.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { ColmeiaRevisao, Colmeia } from '../../../app/shared/sdk';
 
 
 @IonicPage()
@@ -9,11 +10,35 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class ColmeiaRevisaoFormPage {
 
+  public dadosDoForm: ColmeiaRevisao | any = new ColmeiaRevisao();
+  public colmeia: Colmeia = new Colmeia();
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+    let item = navParams.get('item');
+    if (item) {
+      this.dadosDoForm = item;
+    } else {
+      this.dadosDoForm.quantidadeOvo = 0;
+      this.dadosDoForm.quantidadeLarva = 0;
+      this.dadosDoForm.quantidadePupa = 0;
+      this.dadosDoForm.quantidadePupaZangao = 0;
+      this.dadosDoForm.quantidadePolen = 0;
+      this.dadosDoForm.quantidadeNectar = 0;
+      this.dadosDoForm.quantidadeMel = 0;
+    }
+    let colmeia = navParams.get('colmeia');
+    if (colmeia) this.colmeia = colmeia;
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad ColmeiaRevisaoFormPage');
+  addUm(variavel: string) {
+    if (!this.dadosDoForm[variavel]) this.dadosDoForm[variavel] = 0;
+    this.dadosDoForm[variavel] += 1;
+    console.log('addUm', this.dadosDoForm[variavel]);
+  }
+
+  subUm(variavel: string) {
+    if (!this.dadosDoForm[variavel]) this.dadosDoForm[variavel] = 1;
+    this.dadosDoForm[variavel] -= 1;
   }
 
 }
