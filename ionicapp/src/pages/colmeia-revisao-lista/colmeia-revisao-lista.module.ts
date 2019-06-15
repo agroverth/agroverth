e2dfcab7da869fb69098e9f9e4dac2bd3e3f9340
Veloso <desenvolvedoroficial@gmail.com>
2019-06-15 08:34:26 -0300

    colmeia revisao frontend

---
+++ ionicapp/src/pages/colmeia-revisao-lista/colmeia-revisao-lista.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { ColmeiaRevisaoListaPage } from './colmeia-revisao-lista';
+
+@NgModule({
+  declarations: [
+    ColmeiaRevisaoListaPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(ColmeiaRevisaoListaPage),
+  ],
+})
+export class ColmeiaRevisaoListaPageModule {}
