d80ba0290770b9b160af096336860f2884c552c0
Veloso <desenvolvedoroficial@gmail.com>
2019-06-15 08:28:10 -0300

    colmeia form backend

---
+++ ionicapp/src/pages/colmeia-form/colmeia-form.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { ColmeiaFormPage } from './colmeia-form';
+
+@NgModule({
+  declarations: [
+    ColmeiaFormPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(ColmeiaFormPage),
+  ],
+})
+export class ColmeiaFormPageModule {}
