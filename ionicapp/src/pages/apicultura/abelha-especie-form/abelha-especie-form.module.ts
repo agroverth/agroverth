3a4bb736e1617e77ac865ca41181a10b1492d575
Veloso <desenvolvedoroficial@gmail.com>
2019-06-15 22:28:02 -0300

    front end criar form abelha especie

---
+++ ionicapp/src/pages/apicultura/abelha-especie-form/abelha-especie-form.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { AbelhaEspecieFormPage } from './abelha-especie-form';
+
+@NgModule({
+  declarations: [
+    AbelhaEspecieFormPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(AbelhaEspecieFormPage),
+  ],
+})
+export class AbelhaEspecieFormPageModule {}
